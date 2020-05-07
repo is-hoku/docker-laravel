1e5eeb88c86057cf5243e416fc8661bfe64d25a6
Ishibe Hoku <nono@localhost.localdomain>
2020-05-07 20:56:16 +0900

    delete button

--- php/techbookk/resources/views/oya.blade.php
+++ php/techbookk/resources/views/oya.blade.php
@@ -13,16 +13,16 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="/techbook">TechBook</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="/techbook">Home</a>
-            <a class="nav-item nav-link active" href="/techbook/statistics">Statistics</a>
+{{--    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">--}}
+{{--        <span class="navbar-toggler-icon"></span>--}}
+{{--    </button>--}}
+{{--    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">--}}
+{{--        <div class="navbar-nav">--}}
+{{--            <a class="nav-item nav-link active" href="/techbook">Home</a>--}}
+{{--            <a class="nav-item nav-link active" href="/techbook/statistics">Statistics</a>--}}
 {{--            <a class="nav-item nav-link active" href="/techbook/profile">Profile</a>--}}
 {{--            <a class="nav-item nav-link active" href="/techbook/login">Login</a>--}}
-        </div>
+{{--        </div>--}}
     </div>
 </nav>
 @yield('home')
